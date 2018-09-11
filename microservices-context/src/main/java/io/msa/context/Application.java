9405646eba8f78e967be4f679e3b34f9e22bf1c7
hari kishore <mhari.cloud@gmail.com>
2018-09-11 17:53:40 +0530

    changes

--- microservices-context/src/main/java/io/msa/context/Application.java
+++ microservices-context/src/main/java/io/msa/context/Application.java
@@ -2,7 +2,6 @@ package io.msa.context;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
 @SpringBootApplication
-@EnableJpaRepositories
 @EnableDiscoveryClient
 public class Application extends RepositoryRestMvcConfiguration {
 
