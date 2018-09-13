28745083e94607e0687f85ae3a25c8c4ee8fd4bf
HARI KISHORE <mhari.devoxx@gmail.com>
2018-09-13 10:37:40 +0530

    adding context microservices

--- microservices-context/src/main/java/io/msa/context/Application.java
+++ microservices-context/src/main/java/io/msa/context/Application.java
@@ -2,6 +2,7 @@ package io.msa.context;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
 @SpringBootApplication
+@EnableJpaRepositories
 @EnableDiscoveryClient
 public class Application extends RepositoryRestMvcConfiguration {
 
